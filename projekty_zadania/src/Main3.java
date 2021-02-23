43156f36190ca5178f84a24b8498f556684a50be
drobileuu <igor-03.03@o2.pl>
2021-02-23 14:39:57 +0100

    Create Main3.java

---
+++ projekty_zadania/src/Main3.java
@@ -0,0 +1,9 @@
+public class Main3 {
+    public static void main(String[] args){
+        int bok = 7;
+        double wysokosc = bok * Math.sqrt(3) / 2;
+        double Pole = bok * wysokosc / 2;
+
+        System.out.println("Pole trójkąta równobocznego =" + Pole);
+    }
+}

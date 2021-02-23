ac0c810297734cfbce434c47d41a4561ebda59b1
drobileuu <igor-03.03@o2.pl>
2021-02-23 14:39:53 +0100

    Create Main1.java

---
+++ projekty_zadania/src/Main1.java
@@ -0,0 +1,9 @@
+public class Main1 {
+    public static void main(String[] args){
+        int intA = 5;
+        int intB = 8;
+        int wynik =  intA * intA + intB * intB;
+
+        System.out.println("Wynik =" + wynik);
+    }
+}

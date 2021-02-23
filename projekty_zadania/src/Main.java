58fd78a86b6dad5165e0508ea4537b12038bb81d
drobileuu <igor-03.03@o2.pl>
2021-02-23 14:39:48 +0100

    Create Main.java

---
+++ projekty_zadania/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+
+
+        int intA = 4;
+        double doubleB = 4.6;
+        char charC = 'k';
+        String stringD = "Dobrego dnia";
+
+        System.out.println("int =" + intA);
+        System.out.println("double =" + doubleB);
+        System.out.println("char =" + charC);
+        System.out.println("string =" + stringD);
+    }
+}

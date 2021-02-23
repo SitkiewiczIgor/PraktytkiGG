548ce29a23e1f11d081062352b2d1bc67189deaa
drobileuu <igor-03.03@o2.pl>
2021-02-23 14:39:54 +0100

    Create Main2.java

---
+++ projekty_zadania/src/Main2.java
@@ -0,0 +1,9 @@
+public class Main2 {
+    public static void main(String[] args){
+        String imie = "Igor";
+        String nazwisko = "Sitkiewicz";
+
+        System.out.println("imie =" + imie);
+        System.out.println("nazwisko =" + nazwisko);
+    }
+}

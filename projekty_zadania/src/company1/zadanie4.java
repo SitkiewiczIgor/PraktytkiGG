c2b8106e75a5619b8b4d40ff99e2d0762aabeec6
drobileuu <igor-03.03@o2.pl>
2021-03-07 15:48:35 +0100

    Create zadanie4.java

---
+++ projekty_zadania/src/company1/zadanie4.java
@@ -0,0 +1,10 @@
+package com.company1;
+
+public class zadanie4 {
+    public static void main(String[] args){
+        Piesio piesio = new Piesio();
+        piesio.bawsie();
+        piesio.badzmily();
+        System.out.println();
+    }
+}

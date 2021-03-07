d9ec207e5921d38ca3ba49fe958a76f1d54868b5
drobileuu <igor-03.03@o2.pl>
2021-03-07 15:48:29 +0100

    Create zadanie1.java

---
+++ projekty_zadania/src/company1/zadanie1.java
@@ -0,0 +1,12 @@
+package com.company1;
+
+public class zadanie1 {
+    public static void main(String[] args){
+        Piesio pies = new Piesio();
+        pies.wiek();
+        pies.wydajDzwiek();
+        pies.sen();
+        pies.Ciągnięcie();
+        System.out.println();
+    }
+}

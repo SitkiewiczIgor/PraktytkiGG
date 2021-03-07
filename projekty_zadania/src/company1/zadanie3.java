d2661874f07a11a63d4ddb7c293aace47382b218
drobileuu <igor-03.03@o2.pl>
2021-03-07 15:48:34 +0100

    Create zadanie3.java

---
+++ projekty_zadania/src/company1/zadanie3.java
@@ -0,0 +1,13 @@
+package com.company1;
+
+public class zadanie3 {
+    public static void main(String[] args){
+        Rocznikowanie rocznikowanie = new Rocznikowanie();
+        rocznikowanie.bulgocze();
+        rocznikowanie.nalewanie();
+        rocznikowanie.rocznik();
+        rocznikowanie.odkorkowywanie();
+        rocznikowanie.picie();
+        System.out.println();
+    }
+}

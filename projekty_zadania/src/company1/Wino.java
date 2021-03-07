2ea3c29cd00a880f7abbd2ce58c4026889c2ba26
drobileuu <igor-03.03@o2.pl>
2021-03-07 15:48:27 +0100

    Create Wino.java

---
+++ projekty_zadania/src/company1/Wino.java
@@ -0,0 +1,12 @@
+package com.company1;
+
+abstract public class Wino {
+    abstract void bulgocze();
+    abstract void nalewanie();
+    void picie(){
+        System.out.println("Jest Pite");
+    }
+    void odkorkowywanie(){
+        System.out.println("Odkorkowuje sie");
+    }
+}

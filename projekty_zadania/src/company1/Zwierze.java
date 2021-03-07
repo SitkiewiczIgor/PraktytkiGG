8c6db3dfff798ad3732e7222962b48c639a563dc
drobileuu <igor-03.03@o2.pl>
2021-03-07 15:48:39 +0100

    Create Zwierze.java

---
+++ projekty_zadania/src/company1/Zwierze.java
@@ -0,0 +1,15 @@
+package com.company1;
+
+abstract public class Zwierze {
+    abstract public int wiek();
+
+    abstract public void wydajDzwiek();
+
+    public void sen() {
+        System.out.println("Zzzzzz");
+    }
+
+    public void Ciągnięcie() {
+        System.out.println("ciągnie");
+    }
+}

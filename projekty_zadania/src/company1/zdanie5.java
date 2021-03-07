a3d4fb77d7a0c1d70dbbee5f2a31e177ce5719d4
drobileuu <igor-03.03@o2.pl>
2021-03-07 15:48:37 +0100

    Create zdanie5.java

---
+++ projekty_zadania/src/company1/zdanie5.java
@@ -0,0 +1,8 @@
+package com.company1;
+
+public class zdanie5 {
+    public static void main(String[] args){
+        Kwadrat kwadrat = new Kwadrat(10);
+        System.out.println("Obw = "+kwadrat.obliczObw()+"\n"+"Pole = "+kwadrat.obliczPole());
+    }
+}
